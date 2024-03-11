0eedb2162e9bd844f90902e70b06c46ff204eb3c
Satyam Mittal <mittalcodes@gmail.com>
2024-03-11 14:02:11 +0530

    Persistent data-storage, { MongoDB } is set up

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 
 import { server } from "./index.js";
+import connectToMongoDB from "./src/config/mongodb.js";
 
 
 
 server.listen(3400,function(err)
 {
-    if( err ) console.log(err) ;
-    else console.log("Server is listening on port: 3400") ;
+    if( err ) return console.log(err) ;
+    console.log("Server is listening on port: 3400") ;
+    // as soon as server starts running connect to database
+    connectToMongoDB() ;
 });
---
+++ src/config/mongodb.js
@@ -0,0 +1,19 @@
+
+import { MongoClient } from "mongodb";
+
+const url = 'mongodb://localhost:27017/ecomdb' ;
+
+const connectToMongoDB =() =>
+{
+    MongoClient.connect(url)
+        .then(client=>
+            {
+                console.log("MongoDB is connected") ;
+            })
+        .catch(err=>
+            {
+                console.log(err) ;
+            });
+}
+
+export default connectToMongoDB ;
